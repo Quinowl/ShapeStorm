26fcb6ef91fa37a11246d0e9f320071eb52510ba
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-01-28 12:42:56 +0100

    Added player movement input

---
+++ ShapeStorm/Assets/Core/Managers/InputHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+
+    private InputAction movementInputAction;
+
+    public Vector2 MoveInput { get; private set; }
+
+    private void Awake()
+    {
+        InitializeInputs();
+    }
+
+    private void Update()
+    {
+        ReadInputs();
+    }
+
+    private void InitializeInputs()
+    {
+        movementInputAction = playerInput.actions["Move"];
+    }
+
+    private void ReadInputs()
+    {
+        MoveInput = movementInputAction.ReadValue<Vector2>();
+    }
+}
