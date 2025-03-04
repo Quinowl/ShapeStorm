08c27337b35264b694763400fe29a2709d8cbd1e
Quinowl <127102187+Quinowl@users.noreply.github.com>
2025-03-04 01:06:55 +0100

    Added service locator

--- ShapeStorm/Assets/Shape_Storm/Runtime/Managers/InputHandler.cs
+++ ShapeStorm/Assets/Shape_Storm/Runtime/Managers/InputHandler.cs
@@ -1,31 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputHandler : MonoBehaviour
-{
+public class InputHandler : MonoBehaviour {
+
     [SerializeField] private PlayerInput playerInput;
 
     private InputAction movementInputAction;
 
     public Vector2 MoveInput { get; private set; }
 
-    private void Awake()
-    {
+    private void Awake() {
         InitializeInputs();
     }
 
-    private void Update()
-    {
+    private void Update() {
         ReadInputs();
     }
 
-    private void InitializeInputs()
-    {
+    private void InitializeInputs() {
         movementInputAction = playerInput.actions["Move"];
     }
 
-    private void ReadInputs()
-    {
+    private void ReadInputs() {
         MoveInput = movementInputAction.ReadValue<Vector2>();
     }
 }
---
+++ ShapeStorm/Assets/Shape_Storm/Runtime/Systems/ServiceLocator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+public class ServiceLocator {
+
+    private static ServiceLocator instance;
+    public static ServiceLocator Instance => instance ??= new ServiceLocator();
+
+    private readonly Dictionary<Type, object> services;
+
+    private ServiceLocator() => services = new Dictionary<Type, object>();
+
+    public void RegisterService<T>(T service) {
+        var type = typeof(T);
+        if (!services.ContainsKey(type)) services.Add(type, service);
+    }
+
+    public T GetService<T>() {
+        var type = typeof(T);
+        if (services.TryGetValue(type, out var service)) return (T)service;
+        throw new Exception($"Service {type} not found.");
+    }
+
+    public void UnregisterService<T>() {
+        var type = typeof(T);
+        if (services.ContainsKey(type)) {
+            services.Remove(type);
+            return;
+        }
+        throw new Exception($"Service {type} is not registered.");
+    }
+}
